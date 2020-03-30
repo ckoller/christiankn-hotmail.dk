410b350435afb973244b0674013c63dea16c1d39
ckoller <christiankn@hotmail.dk>
2020-03-30 17:41:16 +0200

    refactor backend into flask_backend folder

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -7,7 +7,6 @@ from modules.index.routes import mod as index_blueprint
 def create_app():
     flask_app = Flask(__name__)
     flask_app.register_blueprint(index_blueprint, url_prefix='/')
-
     flask_app.register_blueprint(alpha_blueprint, url_prefix='/api/alpha')
     return flask_app
 
